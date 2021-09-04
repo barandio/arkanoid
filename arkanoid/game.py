59118d83e487d62b5435439e33b17de24041c3ba
Julia B <barandiaranjulia@hotmail.com>
2021-09-04 10:20:13 +0200

    Initial commit

---
+++ arkanoid/game.py
@@ -0,0 +1,23 @@
+import pygame as pg
+from arkanoid import ALTO, ANCHO
+
+
+pg.init()
+
+class Game():
+    def __init__(self):
+        self.pantalla = pg.display.set_mode((ANCHO, ALTO))
+
+    def launch(self):
+        game_over = False
+        while not game_over:
+            for evento in pg.event.get():
+                if evento.type == pg.QUIT:
+                    game_over = True
+        
+            self.pantalla.fill((123, 123, 255))
+
+            pg.display.flip()
+
+        pg.quit()
+        
